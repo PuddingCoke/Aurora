794467a18ab81fac6c1c3b862c7658bd946eabc5
PuddingCoke <zhangxianzhe1231@outlook.com>
2022-09-09 18:54:09 +0800

    修改了下流体模拟的渲染分辨率，取消了DEBUG模式。

--- FluidSimulation/main.cpp
+++ FluidSimulation/main.cpp
@@ -7,7 +7,7 @@ using EngineUsage = Configuration::EngineUsage;
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
 {
-	Configuration config(hInstance, 1280, 720, L"Aurora Test", CameraType::Orthogonal, 4, EngineUsage::Normal, true);
+	Configuration config(hInstance, 1920, 1080, L"Aurora Test", CameraType::Orthogonal, 4, EngineUsage::Normal, false);
 	if (!Aurora::get().iniEngine(config))
 	{
 		Aurora::get().iniGame(new MyGame());
