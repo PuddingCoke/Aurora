7a258804ebecd430e123ad4a5b2475213c079db2
PuddingCoke <zhangxianzhe1231@outlook.com>
2022-11-18 00:32:54 +0800

    写了一天了，感觉还是老老实实用voxel cone tracing好，screen space reflection需要调整一堆参数.........

--- IntelSponza/MyGame.h
+++ IntelSponza/MyGame.h
@@ -3,6 +3,7 @@
 #include<Aurora/Game.h>
 #include<Aurora/A3D/FPSCamera.h>
 #include<Aurora/A3D/ShadowMap.h>
+#include<Aurora/A3D/TextureCube.h>
 #include<Aurora/PostProcessing/BloomEffect.h>
 #include<Aurora/PostProcessing/HBAOEffect.h>
 
@@ -33,8 +34,6 @@ public:
 
 	RenderTexture* gRoughnessMetallic;
 
-	RenderTexture* gViewSpacePosition;
-
 	RenderTexture* originTexture;
 
 	Shader* deferredVS;
@@ -52,9 +51,9 @@ public:
 
 	Shader* deferredFinal;
 
-	Shader* screenSpaceReflection;
+	Shader* skyboxPS;
 
-	Shader* viewSpaceConvert;
+	TextureCube* skybox;
 
 	BloomEffect bloomEffect;
 
@@ -73,7 +72,6 @@ public:
 		gPosition(new RenderTexture(Graphics::getWidth(), Graphics::getHeight(), DXGI_FORMAT_R32G32B32A32_FLOAT)),
 		gNormal(new RenderTexture(Graphics::getWidth(), Graphics::getHeight(), DXGI_FORMAT_R32G32B32A32_FLOAT)),
 		gRoughnessMetallic(new RenderTexture(Graphics::getWidth(), Graphics::getHeight(), DXGI_FORMAT_R8G8B8A8_UNORM)),
-		gViewSpacePosition(new RenderTexture(Graphics::getWidth(), Graphics::getHeight(), DXGI_FORMAT_R32G32B32A32_FLOAT)),
 		originTexture(new RenderTexture(Graphics::getWidth(), Graphics::getHeight(), DXGI_FORMAT_R16G16B16A16_FLOAT)),
 		deferredVS(new Shader("DeferredVShader.hlsl", ShaderType::Vertex)),
 		deferredPSHasTex(new Shader("DeferredPSHasTex.hlsl", ShaderType::Pixel)),
@@ -81,10 +79,10 @@ public:
 		deferredPSTrans(new Shader("DeferredPSTrans.hlsl", ShaderType::Pixel)),
 		debugPS(new Shader("DebugPS.hlsl", ShaderType::Pixel)),
 		deferredFinal(new Shader("DeferredFinal.hlsl", ShaderType::Pixel)),
-		screenSpaceReflection(new Shader("SSRPS.hlsl", ShaderType::Pixel)),
-		viewSpaceConvert(new Shader("ViewSpaceConvertPS.hlsl", ShaderType::Pixel)),
 		bloomEffect(Graphics::getWidth(), Graphics::getHeight()),
-		hbaoEffect(Graphics::getWidth(), Graphics::getHeight())
+		hbaoEffect(Graphics::getWidth(), Graphics::getHeight()),
+		skybox(new TextureCube("D:/Assets/SpaceShip/360-Space-Panorama-III.hdr", 2048)),
+		skyboxPS(new Shader("SkyboxPS.hlsl", ShaderType::Pixel))
 	{
 		exposure = bloomEffect.getExposure();
 		gamma = bloomEffect.getGamma();
@@ -104,24 +102,6 @@ public:
 			Renderer::device->CreateInputLayout(layout, ARRAYSIZE(layout), SHADERDATA(deferredVS), inputLayout.ReleaseAndGetAddressOf());
 		}
 
-		{
-			D3D11_SAMPLER_DESC desc = {};
-			desc.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
-			desc.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
-			desc.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
-			desc.BorderColor[0] = 0.f;
-			desc.BorderColor[1] = 0.f;
-			desc.BorderColor[2] = 0.f;
-			desc.BorderColor[3] = 1.f;
-			desc.ComparisonFunc = D3D11_COMPARISON_NEVER;
-			desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-			desc.MinLOD = 0.f;
-			desc.MaxLOD = FLT_MAX;
-
-			Renderer::device->CreateSamplerState(&desc, borderSampler.ReleaseAndGetAddressOf());
-
-		}
-
 		camera.registerEvent();
 
 		Camera::setProj(Math::pi / 4.f, Graphics::getAspectRatio(), 1.f, 1024.f);
@@ -138,8 +118,9 @@ public:
 		delete gPosition;
 		delete gNormal;
 		delete gRoughnessMetallic;
-		delete gViewSpacePosition;
 		delete originTexture;
+
+		delete skybox;
 
 		delete deferredVS;
 		delete deferredPSHasTex;
@@ -147,8 +128,7 @@ public:
 		delete deferredPSTrans;
 		delete debugPS;
 		delete deferredFinal;
-		delete screenSpaceReflection;
-		delete viewSpaceConvert;
+		delete skyboxPS;
 	}
 
 	void update(const float& dt) override
@@ -215,43 +195,28 @@ public:
 
 		RenderAPI::get()->DrawQuad();
 
-		gViewSpacePosition->clearRTV(DirectX::Colors::Black);
-		RenderAPI::get()->OMSetRTV({ gViewSpacePosition }, nullptr);
-		RenderAPI::get()->PSSetBuffer({ Camera::getViewBuffer() }, 1);
-		RenderAPI::get()->PSSetSRV({ gPosition }, 0);
-		RenderAPI::get()->PSSetSampler({ States::pointClampSampler }, 0);
-
-		RenderAPI::fullScreenVS->use();
-		viewSpaceConvert->use();
-
-		RenderAPI::get()->DrawQuad();
-
-		RenderAPI::get()->ClearDefRTV(DirectX::Colors::Black);
-		RenderAPI::get()->OMSetDefRTV(nullptr);
-		RenderAPI::get()->PSSetBuffer({ Camera::getProjBuffer(),Camera::getViewBuffer() }, 1);
-		RenderAPI::get()->PSSetSRV({ originTexture,gViewSpacePosition,gNormal }, 0);
-		RenderAPI::get()->PSSetSampler({ borderSampler.Get() }, 0);
-
-		RenderAPI::fullScreenVS->use();
-		screenSpaceReflection->use();
-
-		RenderAPI::get()->DrawQuad();
-
-		/*ShaderResourceView* bloomSRV = bloomEffect.process(originTexture);
+		ShaderResourceView* bloomSRV = bloomEffect.process(originTexture);
 
 		RenderAPI::get()->OMSetBlendState(nullptr);
 
 		RenderAPI::get()->ClearDefRTV(DirectX::Colors::Black);
 		RenderAPI::get()->OMSetDefRTV(nullptr);
-
 		RenderAPI::get()->PSSetSampler({ States::pointClampSampler }, 0);
 		RenderAPI::get()->PSSetSRV({ bloomSRV }, 0);
 
 		RenderAPI::fullScreenVS->use();
 		RenderAPI::fullScreenPS->use();
 
-		RenderAPI::get()->DrawQuad();*/
+		RenderAPI::get()->DrawQuad();
 
+		RenderAPI::get()->OMSetDefRTV(depthView);
+		RenderAPI::get()->PSSetSRV({ skybox }, 0);
+		RenderAPI::get()->PSSetSampler({ States::linearClampSampler }, 0);
+
+		RenderAPI::skyboxVS->use();
+		skyboxPS->use();
+
+		RenderAPI::get()->DrawCube();
 	}
 
 
---
+++ IntelSponza/SkyboxPS.hlsl
@@ -0,0 +1,9 @@
+SamplerState linearSampler : register(s1);
+
+TextureCube skyCube : register(t0);
+
+float4 main(float3 position : POSITION) : SV_TARGET
+{
+    float3 envColor = skyCube.Sample(linearSampler, position).rgb;
+    return float4(envColor, 1.0);
+}
